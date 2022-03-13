9836666765701ee60b7104ceda4d79b14df010d9
sub <crowsmith1011@gmail.com>
2022-03-13 11:08:40 +0300

    add create_app, mv app start to if main block

--- src/app.py
+++ src/app.py
@@ -1,5 +1,12 @@
 import connexion
 
-app = connexion.FlaskApp(__name__, specification_dir='openapi/')
-app.add_api('passwords_manager.openapi.yaml')
-app.run(port=8080)
+def create_app() -> connexion.FlaskApp:
+    """ Create Connexion Flask app """
+    app = connexion.FlaskApp(__name__, specification_dir='openapi/')
+    app.add_api('passwords_manager.openapi.yaml')
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(port=8080)
